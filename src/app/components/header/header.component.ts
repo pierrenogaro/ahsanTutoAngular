aae5aeb711c4361ef6003fc75ae321f89f3383fc
Pierre <pierre.nogaro@gmail.com>
2025-01-19 16:35:39 +0100

    event key

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  title = 'My first angular app'
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {GreetingComponent} from '../components/greeting/greeting.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [GreetingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  message = signal('Hello, world!');
+
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`user pressed the ${event.key} key`);
+  }
 
 }
