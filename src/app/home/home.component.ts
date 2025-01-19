949a6e442a95dd7e9d22b65dd24e19170484961f
Pierre <pierre.nogaro@gmail.com>
2025-01-19 18:10:44 +0100

    routes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import {Component, signal} from '@angular/core';
 import {GreetingComponent} from '../components/greeting/greeting.component';
 import {CounterComponent} from '../components/counter/counter.component';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [GreetingComponent, CounterComponent],
+  imports: [GreetingComponent, CounterComponent, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
